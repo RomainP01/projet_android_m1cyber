be6dafdc05512ffe991b9675eb7e9f03ec944088
bbmt0 <benoit.beaumont@protonmail.com>
2023-05-20 10:45:37 +0200

    wip

--- app/src/main/java/com/sauce_hannibal/projet_android_m1cyber/ui/screens/leaderboard/LeaderboardScreen.kt
+++ app/src/main/java/com/sauce_hannibal/projet_android_m1cyber/ui/screens/leaderboard/LeaderboardScreen.kt
@@ -1,6 +1,7 @@
 package com.sauce_hannibal.projet_android_m1cyber.ui.screens.leaderboard
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.magnifier
 import androidx.compose.material3.Text
@@ -10,6 +11,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -22,9 +25,38 @@ fun LeaderboardScreen(
         modifier = modifier
             .fillMaxWidth()
             .fillMaxHeight(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        verticalArrangement = Arrangement.Top,
     ) {
+        Row(Modifier.fillMaxWidth()
+            .padding(top = 200.dp)
+            .wrapContentSize(Alignment.Center),
+            verticalAlignment = Alignment.Bottom)
+        {
+            Box(
+                modifier = Modifier
+                    .size(85.dp)
+                    .background(Color.Green)
+                    .border(width = 2.dp, color = Color.Black)
+
+            )
+            Spacer(modifier = Modifier.width(5.dp))
+            Box(
+                modifier = Modifier
+                    .size(120.dp)
+                    .background(Color.Red)
+                    .border(width = 2.dp, color = Color.Black)
+            )
+            Spacer(modifier = Modifier.width(5.dp))
+            Box(
+                modifier = Modifier
+                    .size(75.dp)
+                    .background(Color.Blue)
+                    .border(width = 2.dp, color = Color.Black)
+
+            )
+
+        }
+        }
         for (user in uiState.users) {
             Row(
                 modifier = modifier
@@ -50,6 +82,6 @@ fun LeaderboardScreen(
             }
         }
     }
-}
+
 
 
