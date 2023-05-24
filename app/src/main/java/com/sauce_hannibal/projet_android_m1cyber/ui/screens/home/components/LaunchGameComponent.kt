168a02eae323792383bc939821a05fb6752e68f5
bbmt0 <benoit.beaumont@protonmail.com>
2023-05-24 17:08:09 +0200

    font change + other design details

--- app/src/main/java/com/sauce_hannibal/projet_android_m1cyber/ui/screens/home/components/LaunchGameComponent.kt
+++ app/src/main/java/com/sauce_hannibal/projet_android_m1cyber/ui/screens/home/components/LaunchGameComponent.kt
@@ -1,19 +1,33 @@
 package com.sauce_hannibal.projet_android_m1cyber.ui.screens.home.components
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.sauce_hannibal.projet_android_m1cyber.ui.screens.home.HomeRoute
 import com.sauce_hannibal.projet_android_m1cyber.ui.screens.home.HomeUiState
 import com.sauce_hannibal.projet_android_m1cyber.ui.screens.home.HomeViewModel
+import com.sauce_hannibal.projet_android_m1cyber.R
+import com.sauce_hannibal.projet_android_m1cyber.ui.theme.Green100
+import com.sauce_hannibal.projet_android_m1cyber.ui.theme.Purple100
 
 @Composable
 fun LaunchGameComponent(
@@ -27,17 +41,24 @@ fun LaunchGameComponent(
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp),
+                .padding(horizontal = 16.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "Trivia Challenge",
+                text = "TRIVIA CHALLENGE",
                 modifier = Modifier
-                    .align(Alignment.CenterHorizontally),
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 100.dp),
                 color = Color.White,
-                fontSize = 50.sp
-            )
+                style = MaterialTheme.typography.titleLarge.copy(
+                    shadow = Shadow(
+                        color = Purple100,
+                        offset = Offset(1f, 1f),
+                        blurRadius = 5f                    )
+                ),
+                textAlign = TextAlign.Center
+                )
         }
         Column(
             modifier = Modifier
@@ -49,18 +70,29 @@ fun LaunchGameComponent(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Button(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth(),
                 onClick = {
                     navController.navigate(HomeRoute.GAME)
                 },
                 enabled = !homeUiState.isDailyChallengeDone
             ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
-                    Text(text = "Daily Ranked")
-                    Text(text = "10 questions")
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_play_arrow_24),
+                        contentDescription = "launch icon",
+                        tint = Color.White
+                    )
+                    Text(
+                        text = "DAILY RANKED \n 10 QUESTIONS",
+                        modifier = Modifier.weight(1f),
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center
+                    )
                 }
+
             }
         }
     }
--- app/src/main/java/com/sauce_hannibal/projet_android_m1cyber/ui/theme/Type.kt
+++ app/src/main/java/com/sauce_hannibal/projet_android_m1cyber/ui/theme/Type.kt
@@ -2,14 +2,30 @@ package com.sauce_hannibal.projet_android_m1cyber.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.sauce_hannibal.projet_android_m1cyber.R
 
+val TacoCrispy = FontFamily(
+    Font(R.font.taco_crispy)
+)
+val Aubergine = FontFamily(
+    Font(R.font.aubergine)
+)
 val Typography = Typography(
     bodySmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = TacoCrispy,
+        fontWeight = FontWeight.Normal,
+        fontSize = 50.sp,
     )
 )
+
+
