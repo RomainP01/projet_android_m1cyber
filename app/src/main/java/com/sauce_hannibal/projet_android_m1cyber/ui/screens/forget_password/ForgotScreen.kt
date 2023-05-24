11200b6069021ff7ef4e6114f2cc08e74dfb4a2e
Ramyy220 <ramyamr70@gmail.com>
2023-05-24 20:17:55 +0200

    end

--- app/src/main/java/com/sauce_hannibal/projet_android_m1cyber/ui/screens/forget_password/ForgotScreen.kt
+++ app/src/main/java/com/sauce_hannibal/projet_android_m1cyber/ui/screens/forget_password/ForgotScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
@@ -25,6 +26,11 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.sauce_hannibal.projet_android_m1cyber.R
 import com.sauce_hannibal.projet_android_m1cyber.ui.Route
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.TextButton
+
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -33,7 +39,7 @@ fun ForgotScreen(navController: NavHostController) {
     val modifier = Modifier
     val uiState = viewModel.resetPasswordFlow.collectAsState().value
     if (uiState == true) {
-        navController.navigate(Route.HOME)
+        navController.navigate(Route.LOGIN)
     }
 
 
@@ -42,6 +48,9 @@ fun ForgotScreen(navController: NavHostController) {
     var emailValue by remember {
         mutableStateOf("")
     }
+    var showDialog by remember { mutableStateOf(false) }
+
+
 
 
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
@@ -94,7 +103,10 @@ fun ForgotScreen(navController: NavHostController) {
 
                 Spacer(modifier = Modifier.padding(10.dp))
                 Button(
-                    onClick = { viewModel.forgotPassword(emailValue.trim())},
+                    onClick = {
+                        viewModel.forgotPassword(emailValue.trim())
+                        showDialog = true
+                    },
                     modifier = Modifier
                         .fillMaxWidth(8.8f)
                         .height(50.dp)
@@ -102,20 +114,48 @@ fun ForgotScreen(navController: NavHostController) {
                     Text(
                         text = "Send",
                         fontSize = 20.sp,
-                        modifier = Modifier.clickable { })
+                        modifier = Modifier.clickable { }
+                    )
                 }
+
                 Spacer(modifier = Modifier.padding(20.dp))
                 Text(text = "Login Instead",
                     modifier = Modifier.clickable {
                         navController.navigate(Route.LOGIN)
                     })
                 Spacer(modifier = Modifier.padding(20.dp))
+                if (showDialog) {
+                    AlertDialog(
+                        onDismissRequest = {
+                            showDialog = false // Fermer la boîte de dialogue lorsque l'utilisateur la ferme
+                        },
+                        title = {
+                            Text(text = "Mail envoyé")
+                        },
+                        text = {
+                            Text(text = "Le mail a été envoyé avec succès.")
+                        },
+                        confirmButton = {
+                            Button(
+                                onClick = {
+                                    showDialog = false // Fermer la boîte de dialogue lorsque l'utilisateur clique sur le bouton OK
+                                }
+                            ) {
+                                Text(text = "OK")
+                            }
+                        }
+                    )
+                }
+
+            }
+
+
 
             }
 
         }
 
     }
-}
+
 
 
